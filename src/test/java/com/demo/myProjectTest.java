f33c344ae131d39541e6effef08c2852825349fe
Divz1991 <divya-6.k-6@cognizant.com>
2025-11-20 18:45:18 +0530

    Update myProjectTest.java

--- src/test/java/com/demo/myProjectTest.java
+++ src/test/java/com/demo/myProjectTest.java
@@ -1,1 +1,12 @@
-
+package com.demo.myProject;
+import org.junit.Test;
+import static
+  org.junit.Assert.assertTrue;
+public class myProjectTest {
+  @Test
+  public void
+  shouldAnswerWithTrue() {
+    //A Simple test that always passes to ensure reports are generated.
+    assertTrue(true);
+  }
+}
